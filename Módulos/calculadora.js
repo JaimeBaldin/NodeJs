ecbd72c97cf2e5fb9b7882813bf1d1161dc64f95
unknown <jaimedbaldin@gmail.com>
2023-04-15 05:47:48 -0300

    Estudando módulos, criando uma calculadora simples.

---
+++ Módulos/calculadora.js
@@ -0,0 +1,6 @@
+import operacoes from './operacoes.js';
+
+console.log(operacoes.soma(1,2));
+console.log(operacoes.div(10,2));
+console.log(operacoes.sub(10,2));
+console.log(operacoes.mult(10,2));
---
+++ Módulos/operacoes.js
@@ -0,0 +1,16 @@
+const operacoes = {
+    soma: function(a, b) {
+      return a + b;
+    },
+    sub: function(a, b) {
+      return a - b;
+    },
+    mult: function(a, b) {
+      return a * b;
+    },
+    div: function(a, b) {
+      return a / b;
+    }
+  };
+  
+  export default operacoes;
