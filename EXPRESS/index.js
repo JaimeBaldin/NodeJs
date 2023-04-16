de03a8c40ba1108da15aa93967a5bf0da9ebf09d
JaimeBaldin <73400106+JaimeBaldin@users.noreply.github.com>
2023-04-16 06:05:28 -0300

    Passando parametros de uma maneira simples na rota GET

--- EXPRESS/index.js
+++ EXPRESS/index.js
@@ -4,13 +4,25 @@ const app = Express();
 
 
 
-app.get('/', function(req, res) {
+  app.get('/', function(req, res) {
     res.send('Olá, mundo!');
   });
 
-  app.get('/sobre', function(req, res) {
-    res.send('Conhencendo um pouco mais');
+  app.get('/sobre/:empresa?', function(req, res) {
+    var empresa = req.params.empresa;
+
+    if(empresa){
+      res.send("A empresa é " + empresa);
+    }else{
+      res.send("A empresa é o nosso...");
+    }
+
   });
+
+  app.get('/ola/:nome', function(req, res) {
+    res.send("Oi " + req.params.nome);
+  });
+
 
 app.listen(8080,function(erro){
     if(erro){
